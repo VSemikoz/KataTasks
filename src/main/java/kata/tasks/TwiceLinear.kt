1b93ee050675fa7f9b5f707cda813aa20e838e26
VSemikoz <slava.semik@yandex.ru>
2020-08-27 21:28:10 +0300

    add TwiceLinear task

---
+++ src/main/java/kata/tasks/TwiceLinear.kt
@@ -0,0 +1,47 @@
+package kata.tasks
+
+import kotlin.test.assertEquals
+
+/**
+ * Consider a sequence u where u is defined as follows:
+
+The number u(0) = 1 is the first one in u.
+For each x in u, then y = 2 * x + 1 and z = 3 * x + 1 must be in u too.
+There are no other numbers in u.
+Ex: u = [1, 3, 4, 7, 9, 10, 13, 15, 19, 21, 22, 27, ...]
+
+1 gives 3 and 4, then 3 gives 7 and 10, 4 gives 9 and 13, then 7 gives 15 and 22 and so on...
+
+Task:
+Given parameter n the function dbl_linear (or dblLinear...) returns the element u(n) of the ordered (with <) sequence u (so, there are no duplicates).
+
+Example:
+dbl_linear(10) should return 22
+
+Note:
+Focus attention on efficiency
+ */
+fun dblLinear(n: Int): Int = createSeq().drop(n).first()
+
+fun calculate(x: Int): ArrayList<Int> = arrayListOf(2 * x + 1, 3 * x + 1)
+
+fun createSeq() = sequence<Int> {
+    val sortSeq = sortedSetOf(1)
+    while (true) {
+        val minVal = sortSeq.first()
+        yield(minVal)
+        sortSeq.remove(minVal)
+        sortSeq.addAll(calculate(minVal))
+    }
+}
+
+fun main() {
+    println("Fixed Tests dblLinear")
+    testing(dblLinear(10), 22)
+    testing(dblLinear(20), 57)
+    testing(dblLinear(30), 91)
+}
+
+private fun testing(actual: Int, expected: Int) {
+    assertEquals(expected.toLong(), actual.toLong())
+}
