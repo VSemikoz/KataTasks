5d5a3f0ed20fbdee9b497467e88a6cc772d4ccb5
VSemikoz <slava.semik@yandex.ru>
2020-08-27 21:17:13 +0300

    add MaximumSubarraySum task

---
+++ src/main/java/kata/tasks/MaximumSubarraySum.kt
@@ -0,0 +1,28 @@
+package kata.tasks
+
+import kotlin.test.assertEquals
+
+/**
+ * The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+maxSequence(listOf(-2, 1, -3, 4, -1, 2, 1, -5, 4));
+// should be 6: listOf(4, -1, 2, 1)
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+ */
+fun maxSequence(arr: List<Int>): Int {
+    var maxSoFar = 0
+    var maxEndingHere = 0
+    arr.forEach {
+        maxEndingHere += it
+        if (maxSoFar < maxEndingHere) maxSoFar = maxEndingHere
+        if (maxEndingHere < 0) maxEndingHere = 0
+    }
+    return maxSoFar
+}
+
+fun main() {
+    assertEquals(0, maxSequence(emptyList()))
+    assertEquals(6, maxSequence(listOf(-2, 1, -3, 4, -1, 2, 1, -5, 4)))
+}
