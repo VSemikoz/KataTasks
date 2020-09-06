98cd37db8476e70c08eae97c4e90c7de8fddabfb
VSemikoz <slava.semik@yandex.ru>
2020-09-07 01:05:15 +0300

    add PrimesInNumbers description

--- src/main/java/kata/tasks/PrimesInNumbers.kt
+++ src/main/java/kata/tasks/PrimesInNumbers.kt
@@ -2,6 +2,17 @@ package kata.tasks
 
 import junit.framework.Assert.assertEquals
 
+/**
+Given a positive number n > 1 find the prime factor decomposition of n.
+The result will be a string with the following form :
+
+"(p1**n1)(p2**n2)...(pk**nk)"
+where a ** b means a to the power of b
+
+with the p(i) in increasing order and n(i) empty if n(i) is 1.
+
+Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
+ */
 object PrimeDecomp {
 
     fun factors(l: Int): String {
